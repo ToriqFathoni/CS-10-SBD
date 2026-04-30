7cb0a64538bd49f64c0a8d655eab1bd854898188
ToriqFathoni <toriqfathoni01@gmail.com>
2026-04-30 19:07:23 +0700

    perbaiki redis

--- completed_backend/src/database/Redis.js
+++ completed_backend/src/database/Redis.js
@@ -1,8 +1,7 @@
 const Redis = require('ioredis');
 
-const redis = new Redis({
-  host: '127.0.0.1',
-  port: 6379,
-});
+const redisUrl = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+
+const redis = new Redis(redisUrl);
 
 module.exports = redis;
